4860c20ff7dbfdd33d2d169b4e519590505b974a
temps <temps@DESKTOP-7U3BE6C>
2022-01-19 20:51:43 +0300

    Добавьте файлы проекта.

---
+++ pract 5/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Pair pair1;
+        private Pair pair2;
+        private Pair pair3;
+
+        private void calculate(object sender, RoutedEventArgs e)
+        {
+            pair1 = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            pair2 = new Pair(int.Parse(firstP2.Text), int.Parse(secondP2.Text));
+            Pair result = pair1.SumPair(pair2); pair1.SumPair(pair2);
+            rez.Text = result.First + ", " + result.Second;
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП-34, Горшков Илья, Вариант 2. Создать класс Pair (пара чисел). Создать необходимые методы и свойства. " +
+                "Определить метод сложения полей и операцию сложения пар (а, b) + (c, d) = (a + c, b + d)" +
+                "Создать перегруженные методы  для  увеличения полей на 1, сложения трех пар чисел.");
+        }
+
+        private void reset(object sender, RoutedEventArgs e)
+        {
+            rez.Clear();
+            firstP1.Clear();
+            secondP1.Clear();
+            secondP2.Clear();
+            firstP2.Clear();
+            secondP2.Clear();
+            firstP3.Clear();
+            secondP3.Clear();
+        }
+
+        private void calculateThreePair(object sender, RoutedEventArgs e)
+        {
+            pair1 = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            pair2 = new Pair(int.Parse(firstP2.Text), int.Parse(secondP2.Text));
+            pair3 = new Pair(int.Parse(firstP3.Text), int.Parse(secondP3.Text));
+            Pair result = pair1.SumPair(pair2, pair3);
+            rez.Text = result.First + ", " + result.Second;
+        }
+    }
+}
---
+++ pract 5/Pair.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract_5
+{
+    class Pair
+    {
+        private int first;
+        private int second;
+
+        public int First
+        {
+            get { return first; }
+        }
+
+        public int Second
+        {
+            get { return second; }
+        }
+
+        public Pair(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public int Sum()
+        {
+            return first + second;
+        }
+
+        public Pair Increase()
+        {
+            return new Pair(first + 1, second + 1);
+        }
+
+        public Pair SumPair(Pair pair)
+        {
+            return new Pair(first + pair.first, second + pair.second);
+        }
+
+        public Pair SumPair(Pair pair1, Pair pair2)
+        {
+            return new Pair(first + pair1.first + pair2.first, second + pair1.second + pair2.second);
+        }
+    }
+}
